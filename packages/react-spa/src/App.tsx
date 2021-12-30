af8e490155986ad17fc4e3f8ca20d275d967a650
Jason Addleman <jas7457@gmail.com>
2021-12-30 13:16:35 -0500

    Some base routes

--- packages/react-spa/src/App.tsx
+++ packages/react-spa/src/App.tsx
@@ -6,8 +6,19 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
         <Route path="page1" element={<Page1 />} />
         <Route path="page2" element={<Page2 />} />
+        <Route path="react" element={<div>It was found</div>} />
+
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
